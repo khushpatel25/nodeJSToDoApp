6ac53f2d9bb762827979040171ec13e9d242022c
Khush Patel <khush.6657@gmail.com>
2023-04-03 15:06:25 +0530

    fixed engine

--- data/database.js
+++ data/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = () => {
     mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-        .then(() => console.log("Database connected successfully....!"))
+        .then((c) => console.log(`Database connected successfully with ${c.connection.host}....!`))
         .catch((e) => console.log(e))
 }
 
